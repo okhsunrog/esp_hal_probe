6f3cab346de7f3234b712d57367d727ba0c117ec
okhsunrog <me@okhsunrog.ru>
2024-08-08 23:17:14 +0300

    pins probing is implemented

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,21 @@ use esp_hal::{
     system::SystemControl,
     timer::{timg::TimerGroup, OneShotTimer},
 };
+use esp_hal::gpio::{GpioPin, Input, Io, Pull};
 use static_cell::make_static;
+
+use embassy_futures::select::{select, Either};
+
+#[embassy_executor::task]
+async fn monitor_pins(mut input4: Input<'static, GpioPin<4>>, mut input5: Input<'static, GpioPin<5>>) {
+    loop {
+        match select(input4.wait_for_rising_edge(), input5.wait_for_rising_edge()).await {
+            Either::First(_) => info!("Rising edge detected on Pin 4!"),
+            Either::Second(_) => info!("Rising edge detected on Pin 5!"),
+        }
+        Timer::after(Duration::from_millis(100)).await;
+    }
+}
 
 #[main]
 async fn main(spawner: Spawner) {
@@ -33,11 +47,14 @@ async fn main(spawner: Spawner) {
     let timg0 = TimerGroup::new(peripherals.TIMG0, &clocks, None);
     let timers = [OneShotTimer::new(timg0.timer0.into())];
     let timers = make_static!(timers);
+
+    let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
+    let input4: Input<GpioPin<4>> = Input::new(io.pins.gpio4, Pull::Down);
+    let input5: Input<GpioPin<5>> = Input::new(io.pins.gpio5, Pull::Down);
     esp_hal_embassy::init(&clocks, timers);
     info!("Embassy initialized!");
-
-    // TODO: Spawn some tasks
-    let _ = spawner;
+    
+    spawner.spawn(monitor_pins(input4, input5)).unwrap();
 
     // Periodically feed the RWDT watchdog timer when our tasks are not running:
     loop {
