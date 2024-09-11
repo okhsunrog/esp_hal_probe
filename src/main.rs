5ec737c1505b5a0be2b5b07cc8b39e98b4589d04
Данила Горнушко <gornushko@naci.local>
2024-09-11 16:18:18 +0300

    latest embassy + minimal code that builds

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,12 @@
 #![no_std]
 #![no_main]
 
-use bt_hci::controller::ExternalController;
 use defmt::{info, unwrap};
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use esp_hal::{peripherals::TIMG0, timer::timg::TimerGroup, Blocking};
 #[allow(unused)]
-use {defmt_rtt as _, esp_backtrace as _, esp_alloc as _};
-use esp_wifi::ble::controller::asynch::BleConnector;
-use trouble_host::{
-    advertise::{AdStructure, Advertisement, BR_EDR_NOT_SUPPORTED, LE_GENERAL_DISCOVERABLE},
-    attribute::{AttributeTable, CharacteristicProp, Service, Uuid},
-    Address,
-    BleHost,
-    BleHostResources,
-    PacketQos,
-};
+use {defmt_rtt as _, esp_backtrace as _};
 use static_cell::StaticCell;
 
 #[embassy_executor::task]
@@ -31,32 +21,9 @@ async fn run() {
 async fn main(spawner: Spawner) {
     let peripherals = esp_hal::init(esp_hal::Config::default());
 
-    esp_alloc::heap_allocator!(72 * 1024);
-
-    //info!("Init!");
+    info!("Init!");
     // RustRover shows error if I don't write full type here, that's weird
     let timg0: TimerGroup<TIMG0, Blocking> = TimerGroup::new(peripherals.TIMG0);
-    let init = unwrap!(esp_wifi::initialize(
-        esp_wifi::EspWifiInitFor::Ble,
-        timg0.timer0,
-        esp_hal::rng::Rng::new(peripherals.RNG),
-        peripherals.RADIO_CLK,
-    ));
-    
-    let systimer = esp_hal::timer::systimer::SystemTimer::new(peripherals.SYSTIMER)
-        .split::<esp_hal::timer::systimer::Target>();
-    esp_hal_embassy::init(systimer.alarm0);
-    
-    let mut bluetooth = peripherals.BT;
-    let connector = BleConnector::new(&init, &mut bluetooth);
-    let controller: ExternalController<_, 20> = ExternalController::new(connector);
-
-    static HOST_RESOURCES: StaticCell<BleHostResources<8, 8, 256>> = StaticCell::new();
-    let host_resources = HOST_RESOURCES.init(BleHostResources::new(PacketQos::None));
-
-    let mut ble: BleHost<'_, _> = BleHost::new(controller, host_resources);
-
-
     spawner.spawn(run()).ok();
 
     loop {
