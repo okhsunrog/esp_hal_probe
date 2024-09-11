0c767111e1bea7d2cd9addafaa5ab593351c57e3
Данила Горнушко <gornushko@naci.local>
2024-09-11 16:55:55 +0300

    Add BLE and allocator support
    Integrated BLE initialization and added `esp-alloc` for dynamic memory management. Updated `Cargo.toml` and `Cargo.lock` with necessary dependencies, and adjusted `main.rs` to accommodate BLE features and system timer initialization.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 #![no_std]
 #![no_main]
 
+extern crate alloc;
+
 use defmt::{info, unwrap};
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
-use esp_hal::{peripherals::TIMG0, timer::timg::TimerGroup, Blocking};
-#[allow(unused)]
-use {defmt_rtt as _, esp_backtrace as _};
+use esp_hal::{
+    peripherals::TIMG0,
+    rng::Rng,
+    timer::{
+        systimer::{SystemTimer, Target},
+        timg::TimerGroup,
+    },
+    Blocking,
+};
+use esp_wifi::{ble::controller::asynch::BleConnector, initialize, EspWifiInitFor};
 use static_cell::StaticCell;
+#[allow(unused)]
+use {defmt_rtt as _, esp_alloc as _, esp_backtrace as _};
 
 #[embassy_executor::task]
 async fn run() {
@@ -19,11 +30,21 @@ async fn run() {
 
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
+    esp_alloc::heap_allocator!(72 * 1024);
     let peripherals = esp_hal::init(esp_hal::Config::default());
 
-    info!("Init!");
     // RustRover shows error if I don't write full type here, that's weird
     let timg0: TimerGroup<TIMG0, Blocking> = TimerGroup::new(peripherals.TIMG0);
+
+    let init = unwrap!(initialize(
+        EspWifiInitFor::Ble,
+        timg0.timer0,
+        Rng::new(peripherals.RNG),
+        peripherals.RADIO_CLK,
+    ));
+    let systimer = SystemTimer::new(peripherals.SYSTIMER).split::<Target>();
+    esp_hal_embassy::init(systimer.alarm0);
+
     spawner.spawn(run()).ok();
 
     loop {
