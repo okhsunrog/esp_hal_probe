348ecf7403e2f87d47cb0def2bbf429281b8d481
Данила Горнушко <gornushko@naci.local>
2024-10-30 11:36:37 +0300

    test

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,10 @@
 use defmt::info;
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
-use esp_hal::{peripherals::TIMG0, timer::timg::TimerGroup, Blocking};
 #[allow(unused)]
-use {defmt_rtt as _, esp_backtrace as _};
+use esp_backtrace as _;
+use esp_hal::{peripherals::TIMG0, timer::timg::TimerGroup, Blocking};
+use rtt_target::{rtt_init, ChannelMode::*};
 
 #[embassy_executor::task]
 async fn run() {
@@ -18,8 +19,31 @@ async fn run() {
 
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
-    info!("Before peripheral init");
     let peripherals = esp_hal::init(esp_hal::Config::default());
+    let channels = rtt_init! {
+        up: {
+            0: {
+                size: 512,
+                mode: BlockIfFull,
+            }
+            1: {
+                size: 512,
+                mode: BlockIfFull,
+            }
+            2: {
+                size: 2048,
+                mode: BlockIfFull,
+            }
+        }
+        down: {
+            0: {
+                size: 512,
+                mode: BlockIfFull,
+            }
+        }
+    };
+    rtt_target::set_defmt_channel(channels.up.0);
+    rtt_target::set_print_channel(channels.up.1);
     info!("Init!");
     // RustRover shows error if I don't write full type here, that's weird
     let timg0: TimerGroup<TIMG0, Blocking> = TimerGroup::new(peripherals.TIMG0);
