78ac16a7df16be7495d1f8758f7adf3e37d35465
Данила Горнушко <gornushko@naci.local>
2024-10-30 11:54:37 +0300

    update esp-hal + minor refactoring

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 use defmt::info;
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
-use esp_hal::{peripherals::TIMG0, timer::timg::TimerGroup, Blocking};
+use esp_hal::timer::timg::TimerGroup;
 #[allow(unused)]
 use {defmt_rtt as _, esp_backtrace as _};
 
@@ -21,8 +21,7 @@ async fn main(spawner: Spawner) {
     info!("Before peripheral init");
     let peripherals = esp_hal::init(esp_hal::Config::default());
     info!("Init!");
-    // RustRover shows error if I don't write full type here, that's weird
-    let timg0: TimerGroup<TIMG0, Blocking> = TimerGroup::new(peripherals.TIMG0);
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
     esp_hal_embassy::init(timg0.timer0);
 
     spawner.spawn(run()).ok();
