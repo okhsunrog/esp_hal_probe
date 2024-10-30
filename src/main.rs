fe2fca247bb97f8622ed1832a88f5068f60ff506
Данила Горнушко <gornushko@naci.local>
2024-10-30 11:47:52 +0300

    added more channels, still works!

--- src/main.rs
+++ src/main.rs
@@ -26,10 +26,24 @@ async fn main(spawner: Spawner) {
                 size: 1024,
                 name: "defmt",
             }
+            1: {
+                size: 1024,
+                mode: BlockIfFull,
+            }
+            2: {
+                size: 4096,
+                mode: BlockIfFull,
+            }
+        }
+        down: {
+            0: {
+                size: 1024,
+                mode: BlockIfFull,
+            }
         }
     };
     rtt_target::set_defmt_channel(channels.up.0);
-    // rtt_target::set_print_channel(channels.up.1);
+    rtt_target::set_print_channel(channels.up.1);
     info!("Init!");
     // RustRover shows error if I don't write full type here, that's weird
     let timg0: TimerGroup<TIMG0, Blocking> = TimerGroup::new(peripherals.TIMG0);
