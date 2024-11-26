6197be039a00b1f9abe46e45b8c55045b44137f8
okhsunrog <me@okhsunrog.dev>
2024-11-27 00:00:03 +0300

    make it work with the latest esp-hal

--- src/bin/ble_test.rs
+++ src/bin/ble_test.rs
@@ -6,13 +6,8 @@ use defmt::info;
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use esp_hal::{
-    peripherals::TIMG0,
     rng::Rng,
-    timer::{
-        systimer::{SystemTimer, Target},
-        timg::TimerGroup,
-    },
-    Blocking,
+    timer::{systimer::SystemTimer, timg::TimerGroup},
 };
 use esp_wifi::ble::controller::BleConnector;
 use trouble_example_apps::ble_bas_peripheral;
@@ -23,17 +18,19 @@ use {defmt_rtt as _, esp_alloc as _, esp_backtrace as _};
 async fn run() {
     loop {
         info!("Hello world from embassy using esp-hal-async!");
-        Timer::after(Duration::from_millis(1_000)).await;
+        Timer::after(Duration::from_millis(2_000)).await;
     }
 }
+
 #[esp_hal_embassy::main]
-async fn main(_s: Spawner) {
+async fn main(spawner: Spawner) {
     esp_alloc::heap_allocator!(128 * 1024);
     let peripherals = esp_hal::init(esp_hal::Config::default());
     info!("Initializing...");
 
     // RustRover shows error if I don't write full type here, that's weird
-    let timg0: TimerGroup<TIMG0, Blocking> = TimerGroup::new(peripherals.TIMG0);
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+
     let init = esp_wifi::init(
         timg0.timer0,
         Rng::new(peripherals.RNG),
@@ -41,8 +38,9 @@ async fn main(_s: Spawner) {
     )
     .unwrap();
 
-    let systimer = SystemTimer::new(peripherals.SYSTIMER).split::<Target>();
+    let systimer = SystemTimer::new(peripherals.SYSTIMER);
     esp_hal_embassy::init(systimer.alarm0);
+    spawner.spawn(run()).unwrap();
 
     let mut bluetooth = peripherals.BT;
     let connector = BleConnector::new(&init, &mut bluetooth);
